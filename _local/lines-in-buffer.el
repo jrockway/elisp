63510efb1505a4e08a2740fefce86f2419c91fec
Jonathan Rockway <jon@jrock.us>
2007-12-13 05:46:27 -0600

    adding lines-in-buffer

---
+++ _local/lines-in-buffer.el
@@ -0,0 +1,20 @@
+; experiment
+(require 'cl)
+
+(defun get-iterator-over-words-in (buffer)
+  (lexical-let ((buf buffer)(pos 1))
+    (lambda ()
+      (save-excursion
+        (let ((cur (current-buffer)) result)
+          (switch-to-buffer buf)
+          (goto-char pos)
+          (forward-word)
+          (let ((pt (point)))
+            (if (not (eq pos pt))
+                (progn 
+                  (setq result (buffer-substring-no-properties pos pt))
+                  (setq pos pt))))
+          (switch-to-buffer cur) result)))))
+
+(setq f (get-iterator-over-words-in (get-buffer "test buffer")))
+(funcall f)
