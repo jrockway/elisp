8140f8b6d643ce89ec80f25974901f1903665ed8
Jonathan Rockway <jon@jrock.us>
2007-12-13 06:23:10 -0600

    add test-more

---
+++ _local/test-more.el
@@ -0,0 +1,40 @@
+(provide 'test-more)
+
+(defmacro with-temp-string (string &rest body)
+  `(with-temp-buffer (insert-string ,string) ,@body))
+
+(defmacro cons-eq (a b)
+  (let ((atmp (make-symbol "a"))
+        (btmp (make-symbol "b")))
+    `(let ((,atmp ,a)
+           (,btmp ,b))
+       (and (eq (car ,atmp) (car ,btmp))
+            (eq (cdr ,atmp) (cdr ,btmp))))))
+
+(defmacro push-test (var)
+  `(setq ,var (append ,var (list (cons counter message)))))
+
+(defun runtests (&rest tests)
+  (let ((buf (or (get-buffer "*runtests*") 
+                 (get-buffer-create "*runtests*")))
+        (counter 0)
+        pass fail)
+    (switch-to-buffer-other-window buf)
+    (goto-char (point-max))
+    (while tests
+      (let ((test (car tests)))
+        (setq counter (+ 1 counter))
+        (setq tests (cdr tests))
+        (let ((passed (car test)) 
+              (message (cdr test)))
+          (insert-string 
+           (format 
+            (if message "%s %d - %s\n" "%s %d\n") 
+            (if passed (progn (push-test pass) "ok")
+              (progn (push-test fail) "not ok")) 
+            counter message)))))
+    (insert-string (format "Result: %s\n\n" (if (not fail) "PASS" "FAIL")))
+    (list pass fail)))
+
+(defun is-deeply (a b &optional message)
+  (cons (cons-eq a b) message))
