aff09c7689c4edcefd83440c473f3c7844ddfc50
Jonathan Rockway <jon@jrock.us>
2007-12-19 03:39:25 -0600

    add cpan-install command

--- _local/cperl-extras.el
+++ _local/cperl-extras.el
@@ -73,4 +73,21 @@
       (end-of-line)
       (insert (concat "\nuse " (add-semicolon module))))))
 
+(defun insert-self-shift (noshift)
+  (if noshift
+      (insert-string "my ($self) = @_;\n")
+    (insert-string "my $self = shift;\n"))
+    (cperl-indent))
+
+(defun cpan-install ()
+  (interactive)
+  (let ((module (read-with-default "Module" (thing-at-point 'perl-module)
+                                   "You must specify a module to install!")))
+
+    (start-process "cpan-process" "*cpan-install*"
+                   "/home/jon/perl/install/bin/cpanp" "install" module)
+    (show-buffer (split-window) "*cpan-install*")))
+
+(global-set-key "\C-c\C-i" 'cpan-install)
+
 (provide 'cperl-extras)
