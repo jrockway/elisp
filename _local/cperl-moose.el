7549e5a4fcc060af5e9d9158f86f4cedab942e38
Jonathan Rockway <jon@jrock.us>
2008-01-03 22:02:21 -0600

    s/use strict; use warnings/use Moose/ or reverse

---
+++ _local/cperl-moose.el
@@ -0,0 +1,9 @@
+
+(defun swap-strict-and-moose ()
+  (interactive)
+  "Changes `use strict; use warnings' to `use Moose' or the reverse"
+  (save-excursion-rewind 
+   (when (re-search-forward "use strict;\nuse warnings;" nil t)
+     (replace-match "use Moose;"))
+   (when (re-search-forward "use Moose;" nil t)
+     (replace-match "use strict;\nuse warnings;"))))
