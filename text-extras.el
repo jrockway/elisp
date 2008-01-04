b66d9581529b04b98ad8b5d03b2b05b960380c8f
Jonathan Rockway <jon@jrock.us>
2008-01-03 22:01:18 -0600

    add function to insert underlines, etc.

---
+++ text-extras.el
@@ -0,0 +1,21 @@
+(defun number-of-chars-in-line-above (&optional position direction-function)
+  "Return the number of characters in the line above.  If
+optional POSITION is specified, goto that character first.  If
+optional DIRECTION-FUNCTION is specified, use that function to
+get to the line (instead of the default `previous-line')."
+  (save-excursion
+    (if position (goto-char position))
+    (if (not direction-function) (setq direction-function 'previous-line))
+    (funcall direction-function)
+    (let (b e)
+      (beginning-of-line)
+      (setq b (point))
+      (end-of-line)
+      (setq e (point))
+      (- e b))))
+
+(defun insert-same-number-of-chars-as-line-above (char)
+  (interactive "kCharacter: ")
+  (dotimes (i (number-of-chars-in-line-above)) (insert char)))
+
+(provide 'text-extras)
