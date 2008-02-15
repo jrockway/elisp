0e5f2e7c184980aad5f342d6ca93f59edcc65e34
Jonathan Rockway <jon@jrock.us>
2008-02-15 02:14:59 -0600

    add reset-trerreset method for erc track

--- _local/erc-extras.el
+++ _local/erc-extras.el
@@ -2,5 +2,11 @@
   (let (iswitchb-buffer-ignore)
     ad-do-it))
 (ad-activate 'erc-iswitchb)
+;; Clears out annoying erc-track-mode stuff for when we don't care.
+;; Useful for when ChanServ restarts :P
+(defun reset-erc-track-mode ()
+  (interactive)
+  (setq erc-modified-channels-alist nil)
+  (erc-modified-channels-update))
 
 (provide 'erc-extras)
