4e40b50562083cc579cdf49a9483cdb023ae18ab
Jonathan Rockway <jon@jrock.us>
2008-03-14 10:00:17 -0500

    add function to bury erc buffers

--- _local/erc-extras.el
+++ _local/erc-extras.el
@@ -2,11 +2,17 @@
   (let (iswitchb-buffer-ignore)
     ad-do-it))
 (ad-activate 'erc-iswitchb)
-;; Clears out annoying erc-track-mode stuff for when we don't care.
-;; Useful for when ChanServ restarts :P
+
 (defun reset-erc-track-mode ()
   (interactive)
   (setq erc-modified-channels-alist nil)
   (erc-modified-channels-update))
+
+(defun erc-go-away nil
+  (interactive)
+  (loop for buf in (buffer-list) 
+        when (with-current-buffer buf (eq 'erc-mode major-mode))
+        do (bury-buffer buf)))
+(erc-go-away)
 
 (provide 'erc-extras)
