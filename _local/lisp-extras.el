714e98788e9fba3deee97a774935e4ff1d74dff3
Jonathan Rockway <jon@jrock.us>
2008-10-04 18:13:29 -0500

    rebind 90() to ()90 in LISP mode

--- _local/lisp-extras.el
+++ _local/lisp-extras.el
@@ -1,7 +1,9 @@
-(add-hook 'lisp-mode-hook
-          (lambda nil (interactive)
-            (local-set-key (kbd "(") (lambda nil (interactive) (insert "[")))
-            (local-set-key (kbd ")") (lambda nil (interactive) (insert "]")))
-            (local-set-key (kbd "[") (lambda nil (interactive) (insert "(")))
-            (local-set-key (kbd "]") (lambda nil (interactive) (insert ")")))))
+(loop for mode in '(lisp-mode-hook emacs-lisp-mode-hook) do
+      (add-hook mode
+                (lambda nil (interactive)
+                  (local-set-key (kbd "9") (lambda nil (interactive) (insert "(")))
+                  (local-set-key (kbd "0") (lambda nil (interactive) (insert ")")))
+                  (local-set-key (kbd ")") (lambda nil (interactive) (insert "0")))
+                  (local-set-key (kbd "(") (lambda nil (interactive) (insert "9"))))))
 
+(provide 'lisp-extras)
