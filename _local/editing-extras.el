6383e9828673120ce60a6b30d9e3da9a725c6ff2
Jonathan Rockway <jon@jrock.us>
2008-11-08 23:15:21 -0600

    add visit-this-file

--- _local/editing-extras.el
+++ _local/editing-extras.el
@@ -25,6 +25,22 @@ the current line to the end of the previous line."
   (interactive)
   (save-excursion (kill-word 1) (backward-kill-word 1)))
 
+(defun rename-this-file ()
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (modified-p (buffer-modified-p)))
+    (when (not filename) (error "Not visiting a file!"))
+    (let* ((new-name (read-file-name
+                      (format "Rename %s to: " filename)
+                      (file-name-directory filename)))
+           (new-directory (file-name-directory new-name)))
+      (when (get-buffer new-name) (error "Already visting the destination!"))
+      (make-directory new-directory t)
+      (rename-file filename new-name)
+      (rename-buffer new-name)
+      (set-visited-file-name new-name)
+      (set-buffer-modified-p modified-p))))
+
 (global-set-key (kbd "C-x ,") 'move-line-up)
 (global-set-key (kbd "C-x .") 'move-next-line-up)
 (global-set-key (kbd "M-D") 'erase-current-word)
