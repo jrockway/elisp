f6b61c3f38e10547882c501e11d8236a2d88a0b2
Jonathan Rockway <jon@jrock.us>
2008-11-08 23:15:14 -0600

    remove lisp keybindings

--- _local/lisp-extras.el
+++ _local/lisp-extras.el
@@ -1,9 +1,1 @@
-(loop for mode in '(lisp-mode-hook emacs-lisp-mode-hook) do
-      (add-hook mode
-                (lambda nil (interactive)
-                  (local-set-key (kbd "9") (lambda nil (interactive) (insert "(")))
-                  (local-set-key (kbd "0") (lambda nil (interactive) (insert ")")))
-                  (local-set-key (kbd ")") (lambda nil (interactive) (insert "0")))
-                  (local-set-key (kbd "(") (lambda nil (interactive) (insert "9"))))))
-
 (provide 'lisp-extras)
