123e30d0b494a430f9e747e46caac2d633b9f5a8
Jonathan Rockway <jon@jrock.us>
2008-11-30 04:20:39 -0600

    remap [] to (); add project support for lisp projects

--- _local/lisp-extras.el
+++ _local/lisp-extras.el
@@ -1,4 +1,22 @@
 
 (defalias 'clhs 'hyperspec-lookup "CLHS is easier to type")
 
+(defun lisp-remap-keys ()
+  (interactive)
+  (local-set-key (kbd "[")
+                 (lambda () (interactive) (insert "(")))
+  (local-set-key (kbd "]")
+                 (lambda () (interactive) (insert ")"))))
+
+(add-hook 'lisp-mode-hook #'lisp-remap-keys)
+(add-hook 'emacs-lisp-mode-hook #'lisp-remap-keys)
+(add-hook 'ielm-mode-hook #'lisp-remap-keys)
+
+(define-project-type lisp (generic)
+  (eproject--scan-parents-for file
+    (lambda (directory)
+      (let ((dname (file-name-nondirectory directory)))
+        (file-exists-p (format "%s/%s.asd" directory dname)))))
+  ("\\.lisp$" "\\.asd"))
+
 (provide 'lisp-extras)
