f9c911ff700fa6e3395c8393e3e018a6b9c0d44b
Jonathan Rockway <jon@jrock.us>
2009-02-15 02:05:00 -0600

    update haskell-mode

--- haskell-mode/haskell-site-file.el
+++ haskell-mode/haskell-site-file.el
@@ -6,19 +6,96 @@
               (or (file-name-directory load-file-name) (car load-path)))
 
 
+
+;;;### (autoloads (haskell-c-mode) "haskell-c" "haskell-c.el" (18170
+;;;;;;  47169))
+;;; Generated autoloads from haskell-c.el
+
+(add-to-list 'auto-mode-alist '("\\.hsc\\'" . haskell-c-mode))
+
+(autoload 'haskell-c-mode "haskell-c" "\
+Major mode for Haskell FFI files.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (haskell-cabal-mode) "haskell-cabal" "haskell-cabal.el"
+;;;;;;  (18231 1867))
+;;; Generated autoloads from haskell-cabal.el
+
+(add-to-list 'auto-mode-alist '("\\.cabal\\'" . haskell-cabal-mode))
+
+(autoload 'haskell-cabal-mode "haskell-cabal" "\
+Major mode for Cabal package description files.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (haskell-decl-scan-mode) "haskell-decl-scan" "haskell-decl-scan.el"
+;;;;;;  (18271 24057))
+;;; Generated autoloads from haskell-decl-scan.el
+
+(autoload 'haskell-decl-scan-mode "haskell-decl-scan" "\
+Minor mode for declaration scanning for Haskell mode.
+Top-level declarations are scanned and listed in the menu item \"Declarations\".
+Selecting an item from this menu will take point to the start of the
+declaration.
+
+\\[haskell-ds-forward-decl] and \\[haskell-ds-backward-decl] move forward and backward to the start of a declaration.
+
+Under XEmacs, the following keys are also defined:
+
+\\[fume-list-functions] lists the declarations of the current buffer,
+\\[fume-prompt-function-goto] prompts for a declaration to move to, and
+\\[fume-mouse-function-goto] moves to the declaration whose name is at point.
+
+This may link with `haskell-doc' (only for Emacs currently).
+
+For non-literate and LaTeX-style literate scripts, we assume the
+common convention that top-level declarations start at the first
+column.  For Bird-style literate scripts, we assume the common
+convention that top-level declarations start at the third column,
+ie. after \"> \".
+
+Anything in `font-lock-comment-face' is not considered for a
+declaration.  Therefore, using Haskell font locking with comments
+coloured in `font-lock-comment-face' improves declaration scanning.
+
+To turn on declaration scanning for all Haskell buffers, add this to
+.emacs:
+
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
+
+To turn declaration scanning on for the current buffer, call
+`turn-on-haskell-decl-scan'.
+
+Literate Haskell scripts are supported: If the value of
+`haskell-literate' (automatically set by the Haskell mode of
+Moss&Thorn) is `bird', a Bird-style literate script is assumed.  If it
+is nil or `tex', a non-literate or LaTeX-style literate script is
+assumed, respectively.
+
+Invokes `haskell-decl-scan-mode-hook'.
+
+\(fn &optional ARG)" nil nil)
+
+;;;***
+
 ;;;### (autoloads (haskell-doc-show-type haskell-doc-mode) "haskell-doc"
-;;;;;;  "haskell-doc.el" (17263 33718))
+;;;;;;  "haskell-doc.el" (18271 24003))
 ;;; Generated autoloads from haskell-doc.el
 
-(autoload (quote haskell-doc-mode) "haskell-doc" "\
+(autoload 'haskell-doc-mode "haskell-doc" "\
 Enter `haskell-doc-mode' for showing fct types in the echo area.
 See variable docstring.
 
 \(fn &optional ARG)" t nil)
 
-(defalias (quote turn-on-haskell-doc-mode) (quote haskell-doc-mode))
+(defalias 'turn-on-haskell-doc-mode 'haskell-doc-mode)
 
-(autoload (quote haskell-doc-show-type) "haskell-doc" "\
+(autoload 'haskell-doc-show-type "haskell-doc" "\
 Show the type of the function near point.
 For the function under point, show the type in the echo area.
 This information is extracted from the `haskell-doc-prelude-types' alist
@@ -30,10 +107,10 @@ current buffer.
 ;;;***
 
 ;;;### (autoloads (haskell-indent-mode) "haskell-indent" "haskell-indent.el"
-;;;;;;  (17263 45887))
+;;;;;;  (18271 24057))
 ;;; Generated autoloads from haskell-indent.el
 
-(autoload (quote haskell-indent-mode) "haskell-indent" "\
+(autoload 'haskell-indent-mode "haskell-indent" "\
 ``intelligent'' Haskell indentation mode that deals with
 the layout rule of Haskell.  \\[haskell-indent-cycle] starts the cycle
 which proposes new possibilities as long as the TAB key is pressed.
@@ -66,21 +143,17 @@ Invokes `haskell-indent-hook' if not nil.
 
 ;;;***
 
-;;;### (autoloads (literate-haskell-mode haskell-mode) "haskell-mode"
-;;;;;;  "haskell-mode.el" (17259 57505))
+;;;### (autoloads (haskell-hoogle literate-haskell-mode haskell-mode)
+;;;;;;  "haskell-mode" "haskell-mode.el" (18271 25098))
 ;;; Generated autoloads from haskell-mode.el
 
-(autoload (quote haskell-mode) "haskell-mode" "\
-Major mode for editing Haskell programs.  Last adapted for Haskell 1.4.
+(autoload 'haskell-mode "haskell-mode" "\
+Major mode for editing Haskell programs.
 Blank lines separate paragraphs, comments start with `-- '.
-
-\\<haskell-mode-map>\\[indent-for-comment] will place a comment at an appropriate place on the current line.
-\\[comment-region] comments (or with prefix arg, uncomments) each line in the region.
-
-Literate scripts are supported via `literate-haskell-mode'.  The
-variable `haskell-literate' indicates the style of the script in the
-current buffer.  See the documentation on this variable for more
-details.
+\\<haskell-mode-map>
+Literate scripts are supported via `literate-haskell-mode'.
+The variable `haskell-literate' indicates the style of the script in the
+current buffer.  See the documentation on this variable for more details.
 
 Modules can hook in via `haskell-mode-hook'.  The following modules
 are supported with an `autoload' command:
@@ -98,18 +171,18 @@ are supported with an `autoload' command:
      Simple indentation.
 
 Module X is activated using the command `turn-on-X'.  For example,
-`haskell-font-lock' is activated using `turn-on-haskell-font-lock'.
-For more information on a module, see the help for its `turn-on-X'
+`haskell-indent' is activated using `turn-on-haskell-indent'.
+For more information on a module, see the help for its `X-mode'
 function.  Some modules can be deactivated using `turn-off-X'.  (Note
 that `haskell-doc' is irregular in using `turn-(on/off)-haskell-doc-mode'.)
 
 Use `haskell-version' to find out what version this is.
 
-Invokes `haskell-mode-hook' if not nil.
+Invokes `haskell-mode-hook'.
 
 \(fn)" t nil)
 
-(autoload (quote literate-haskell-mode) "haskell-mode" "\
+(autoload 'literate-haskell-mode "haskell-mode" "\
 As `haskell-mode' but for literate scripts.
 
 \(fn)" t nil)
@@ -118,29 +191,72 @@ As `haskell-mode' but for literate scripts.
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 
+(autoload 'haskell-hoogle "haskell-mode" "\
+Do a Hoogle search for QUERY.
+
+\(fn QUERY)" t nil)
+
 ;;;***
 
-;;;### (autoloads (inferior-haskell-load-file switch-to-haskell)
-;;;;;;  "inf-haskell" "inf-haskell.el" (17259 53134))
+;;;### (autoloads (inferior-haskell-find-haddock inferior-haskell-find-definition
+;;;;;;  inferior-haskell-info inferior-haskell-type inferior-haskell-load-file
+;;;;;;  switch-to-haskell) "inf-haskell" "inf-haskell.el" (18231
+;;;;;;  1814))
 ;;; Generated autoloads from inf-haskell.el
 
-(defalias (quote run-haskell) (quote switch-to-haskell))
+(defalias 'run-haskell 'switch-to-haskell)
 
-(autoload (quote switch-to-haskell) "inf-haskell" "\
+(autoload 'switch-to-haskell "inf-haskell" "\
 Show the inferior-haskell buffer.  Start the process if needed.
 
 \(fn &optional ARG)" t nil)
 
-(autoload (quote inferior-haskell-load-file) "inf-haskell" "\
+(autoload 'inferior-haskell-load-file "inf-haskell" "\
 Pass the current buffer's file to the inferior haskell process.
+If prefix arg \\[universal-argument] is given, just reload the previous file.
 
 \(fn &optional RELOAD)" t nil)
 
+(autoload 'inferior-haskell-type "inf-haskell" "\
+Query the haskell process for the type of the given expression.
+If optional argument `insert-value' is non-nil, insert the type above point
+in the buffer.  This can be done interactively with the \\[universal-argument] prefix.
+The returned info is cached for reuse by `haskell-doc-mode'.
+
+\(fn EXPR &optional INSERT-VALUE)" t nil)
+
+(autoload 'inferior-haskell-info "inf-haskell" "\
+Query the haskell process for the info of the given expression.
+
+\(fn SYM)" t nil)
+
+(autoload 'inferior-haskell-find-definition "inf-haskell" "\
+Attempt to locate and jump to the definition of the given expression.
+
+\(fn SYM)" t nil)
+
+(autoload 'inferior-haskell-find-haddock "inf-haskell" "\
+Find and open the Haddock documentation of SYM.
+Make sure to load the file into GHCi or Hugs first by using C-c C-l.
+Only works for functions in a package installed with ghc-pkg, or
+whatever the value of `haskell-package-manager-name' is.
+
+This function needs to find which package a given module belongs
+to.  In order to do this, it computes a module-to-package lookup
+alist, which is expensive to compute (it takes upwards of five
+seconds with more than about thirty installed packages).  As a
+result, we cache it across sessions using the cache file
+referenced by `inferior-haskell-module-alist-file'. We test to
+see if this is newer than `haskell-package-conf-file' every time
+we load it.
+
+\(fn SYM)" t nil)
+
 ;;;***
 
-;;;### (autoloads nil nil ("haskell-decl-scan.el" "haskell-font-lock.el"
-;;;;;;  "haskell-ghci.el" "haskell-hugs.el" "haskell-simple-indent.el")
-;;;;;;  (17263 46557 241275))
+;;;### (autoloads nil nil ("haskell-font-lock.el" "haskell-ghci.el"
+;;;;;;  "haskell-hugs.el" "haskell-simple-indent.el") (18271 25750
+;;;;;;  579943))
 
 ;;;***
 
