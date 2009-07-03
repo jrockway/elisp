05e546b3fccf9ec6282ec7d3b3037b36593c0cea
Jonathan Rockway <jon@jrock.us>
2009-07-02 20:17:39 -0700

    eshell extras

---
+++ _local/eshell-extras.el
@@ -0,0 +1,60 @@
+;;; eshell-extras.el --- extra eshell commands
+;;; Code:
+
+(require 'my-macros)
+(require 'eproject)
+
+(defun eshell/clear ()
+  "Clear the screen."
+  (dotimes (i 50) (eshell-print "\n"))
+  nil)
+
+(define-invoke-for-each eshell/byte-compile-file byte-compile-file
+  "Byte-compile the given FILES"
+  files)
+
+(defun eshell/rcd ()
+  "CD to the project root."
+  (let* ((pwd (eshell/pwd))
+         (no-kill-p (get-file-buffer pwd)))
+    (eshell/cd
+     (with-current-buffer (find-file pwd)
+       (prog1 (eproject-root)
+         (when (not no-kill-p)
+           (kill-buffer)))))))
+
+(defvar eproject-project-locations
+  '("/home/jon/projects/"
+    "/home/jon/projects/cpan_modules/"
+    "/home/jon/work")
+  "Places for pcd to look in for projects.")
+
+(defun eshell/pcd (project)
+  "CD to the root of PROJECT."
+
+  (eshell/cd
+   (let+ (eproject-project-root project)
+         (if _
+             _
+           (loop for base in
+                   (mapcar #'file-name-as-directory
+                           eproject-project-locations)
+                 when (file-exists-p (concat base project))
+                 return (concat base project))))))
+
+(defun eshell/magit ()
+  "Run magit status here."
+  (call-interactively #'magit-status)
+  nil)
+
+(defun eshell/loop (&rest args)
+  (eval
+   (cons 'loop
+         (mapcar (lambda (x)
+                   (cond ((stringp x) (read x))
+                         ((listp x) `(quote ,x))
+                         (t x)))
+                 args))))
+
+(provide 'eshell-extras)
+;;; eshell-extras.el ends here
