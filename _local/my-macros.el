6471d3cbd42f27f2cc889a3e0b1209cec6fcb5c2
Jonathan Rockway <jon@jrock.us>
2009-07-02 20:18:48 -0700

    add invoke-for-each macro

--- _local/my-macros.el
+++ _local/my-macros.el
@@ -12,12 +12,29 @@
 ; XXX not really a macro
 (defun read-with-default (string &optional default error)
   (let ((read (read-string
-               (if default 
+               (if default
                    (format "%s (default %s): " string default)
                  (format "%s: " string)))))
     (if (equal read "") (setq read nil))
     (if (and (not read) (not default)) (error error))
     (if (not read) (setq read default))
     read))
+
+(defmacro define-invoke-for-each (name orig &optional docstring varname)
+  "Make a function of one argument into a function of multiple arguments.
+Argument NAME is the name of the generated function.
+Argument ORIG is the original function.
+Optional argument DOCSTRING is the docstring of the generated function.
+Optional argument VARNAME is the name of the argument to the generated function."
+  `(defun ,name (,(or varname 'things))
+     ,(or docstring
+          (format "Invoke %s on each element of THINGS." orig))
+     (loop for thing in ,(or varname 'things) do (,orig thing))))
+
+(defmacro* let+ (to-bind &body forms)
+  "Bind TO-BIND to _ and execute FORM."
+  `(let ((_ ,to-bind))
+     ,@forms))
+
 
 (provide 'my-macros)
