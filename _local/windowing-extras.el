8de738e7c37c4b57aee0e777883a2aaf58c4035e
Jonathan Rockway <jon@jrock.us>
2009-07-28 00:26:27 -0500

    add function to fix window config

--- _local/windowing-extras.el
+++ _local/windowing-extras.el
@@ -13,4 +13,47 @@ the following keybinding in."
 
 (global-set-key (kbd "C-c 4") 'in-other-window)
 
+(defun first-matching-buffer (predicate)
+  "Return PREDICATE applied to the first buffer where PREDICATE applied to the buffer yields a non-nil value."
+  (loop for buf in (buffer-list)
+        when (with-current-buffer buf (funcall predicate buf))
+        return (with-current-buffer buf (funcall predicate buf))))
+
+(defun fix-windows ()
+  "Setup my window config."
+  (interactive)
+  (let ((current-project
+         (first-matching-buffer (lambda (x) (ignore-errors (eproject-name)))))
+        (current-irc-window
+         (first-matching-buffer (lambda (x) (and (eq major-mode 'rcirc-mode)
+                                                 x))))
+        (current-shell
+         (or (first-matching-buffer (lambda (x)
+                                      (and (or (eq major-mode 'eshell-mode)
+                                               (eq major-mode 'term-mode))
+                                           x)))
+             (eshell))))
+
+    (delete-other-windows)
+    (split-window-horizontally)
+    (split-window-horizontally)
+    (window-number-select 1)
+    (split-window-vertically)
+    (labels ((show (x) (set-window-buffer nil (or x (get-buffer-create "*scratch*")))))
+      (window-number-select 1)
+      (show current-irc-window)
+      (window-number-select 2)
+      (show current-shell)
+      (let ((cur))
+        (loop for i in '(3 4)
+              do
+              (window-number-select i)
+              (show (first-matching-buffer
+                     (lambda (x) (and (equal (ignore-errors (eproject-name))
+                                             current-project)
+                                      (not (equal cur (buffer-name x)))
+                                      x))))
+              (setf cur (buffer-name (current-buffer))))))
+    (balance-windows)))
+
 (provide 'windowing-extras)
