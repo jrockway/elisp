bb444d90d65fca7a6631439fb5eba0834606bcb7
Jonathan Rockway <jon@jrock.us>
2010-03-28 08:09:04 -0500

    updates

--- _local/cperl-extras.el
+++ _local/cperl-extras.el
@@ -37,7 +37,8 @@
             (local-set-key "\C-c\C-p" 'ifind-perl-projects)
             (local-set-key "\C-c510" 'kill-5.10)
             (local-set-key "\C-cr" 'cperl-repl)
-            (local-set-key (quote [M-tab]) (quote cperl-reindent-hash))
+            (local-set-key (quote [M-tab]) 'cperl-reindent-hash)
+            (local-set-key (kbd "M-C-i") 'cperl-reindent-hash)
             (local-set-key "\C-cq" 'find-module)))
 
 (add-hook 'tt-mode-hook
