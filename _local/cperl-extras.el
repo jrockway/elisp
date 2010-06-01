83049e84949bf6aadcaf9e8b928b2e7391af5ed9
Jonathan Rockway <jon@jrock.us>
2010-06-01 01:52:09 -0500

    add ability to run last single test; refactor

--- _local/cperl-extras.el
+++ _local/cperl-extras.el
@@ -9,22 +9,46 @@
 (require 'cperl-moose)
 (require 'cperl-reindent)
 
+(defvar cperl-last-test nil
+  "The last test run via `cperl-run-tests-in-eshell'")
+
 (defun cperl-run-tests-in-eshell (&optional prefix)
   (interactive "p")
-  (let* ((name (file-relative-name (buffer-file-name) (eproject-root)))
-         (eshell-buf (eproject-eshell-cd-here prefix)))
-    (with-current-buffer eshell-buf
-      (goto-char (point-max))
-      (eshell-preinput-scroll-to-bottom)
-      (if (string-match ".t$" name)
-          (insert (format "perl -Ilib %s" name))
-        (insert "prove --lib -r -j3 t"))
-      (eshell-send-input nil t)
-      (eshell-postoutput-scroll-to-bottom))))
+  (let ((name (file-relative-name (buffer-file-name) (eproject-root))))
+    (if (string-match ".t$" name)
+        (progn (setq cperl-last-test name)
+               (cperl-run-tests-in-eshell-1 prefix name))
+      (cperl-run-tests-in-eshell-1 prefix))))
+
+(defun cperl-run-last-test-in-eshell (&optional prefix)
+  (interactive "p")
+  (if cperl-last-test
+      (cperl-run-tests-in-eshell-1 prefix cperl-last-test)
+    (cperl-run-tests-in-eshell prefix)))
+
+(defun cperl-run-tests-in-eshell-1 (prefix test)
+  "Run the named test in the visible eshell.
+
+<picture of cat using eshell with caption: VISIBLE ESHELL>.
+
+PREFIX is the prefix arg to pass to `eproject-eshell-cd-here',
+TEST is the test to run, or NIL for all of them."
+  (with-current-buffer (eproject-eshell-cd-here prefix)
+    (eshell-preinput-scroll-to-bottom)
+    (goto-char (point-max))
+    (if test
+        (insert (format "perl -Ilib %s" test))
+      (insert "prove --lib -r -j3 t"))
+    (eshell-send-input nil t)
+    (goto-char (point-max))
+    (ignore-errors
+      (set-window-point (get-buffer-window) (point-max)))))
+
 
 (add-hook 'cperl-mode-hook
           (lambda ()
             (local-set-key (kbd "C-c C-l") 'cperl-run-tests-in-eshell)
+            (local-set-key (kbd "C-c l") 'cperl-run-last-test-in-eshell)
             (local-set-key "\C-ct" 'increment-test-counter)
             (local-set-key "\C-cu" 'add-use)
             (local-set-key "\C-cmu" 'add-Makefile.PL-requires)
@@ -36,7 +60,6 @@
             (local-set-key "\C-c\C-f" 'ifind-perl-project-file)
             (local-set-key "\C-c\C-p" 'ifind-perl-projects)
             (local-set-key "\C-c510" 'kill-5.10)
-            (local-set-key "\C-cr" 'cperl-repl)
             (local-set-key (quote [M-tab]) 'cperl-reindent-hash)
             (local-set-key (kbd "M-C-i") 'cperl-reindent-hash)
             (local-set-key "\C-cq" 'find-module)))
