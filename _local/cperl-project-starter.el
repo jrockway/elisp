0c91bb91d37bbfcd100023a7dd87a382dc108694
Jonathan Rockway <jon@jrock.us>
2010-10-18 04:15:50 -0500

    i never committed this?

--- _local/cperl-project-starter.el
+++ _local/cperl-project-starter.el
@@ -27,43 +27,56 @@
 (require 'magit) ;; <3
 (require 'eproject)
 
+(defvar do-not-want-perl-project nil
+  "Remember `n' answers to 'do you want a project?'.")
+
 (defun already-have-perl-project-p (root)
   (or (string-equal (ignore-errors (eproject-root)) root)
-      (file-exists-p (concat root ".git"))))
+      (or (member root do-not-want-perl-project))
+      (and (file-exists-p (concat root ".git"))
+           (or (file-exists-p (concat root "dist.ini"))
+               (file-exists-p (concat root "Makefile.PL"))
+               (file-exists-p (concat root "Buile.PL"))))))
 
 (defun maybe-init-perl-project ()
   "Create a git + cpan project for the file visited by the current buffer."
   (let* ((filename (buffer-file-name))
-         (root (and (string-match "^\\(.+/\\)lib/.+[.]pm$" filename) ; seeing lib/ ensures that this is really
-                                                                     ; a project and not a one-off thing
+         ;; seeing lib/ ensures that this is really
+         ;; a project and not a one-off thing
+         (root (and (string-match "^\\(.+/\\)lib/.+[.]pm$" filename)
                     (match-string 1 filename))))
-    (when root
-      (let* ((project-name (file-name-nondirectory (directory-file-name root)))
-             (default-directory root))
+  (when (and root (not (already-have-perl-project-p root)))
+    (if (y-or-n-p "Create a new project here? ")
+        (init-perl-project root)
+      (when (not (member root do-not-want-perl-project))
+        (setq do-not-want-perl-project (cons root do-not-want-perl-project)))))))
 
-        (when (not (already-have-perl-project-p root))
-          (magit-init root)
-          (cperl-project-starter-build-gitignore :root root :name project-name)
-          (cperl-project-starter-build-Makefile.PL :root root :all-from filename
-                                                   :name project-name)
-          (cperl-project-starter-build-MANIFEST.SKIP :root root :name project-name)
-          (cperl-project-starter-build-Changes :root root :name project-name)
-          (cperl-project-starter-build-eproject :root root)
+(defun init-perl-project (root)
+  (interactive "DRoot: ")
+  (let ((default-directory root)
+        (project-name (file-name-nondirectory (directory-file-name root))))
+    (when (not (file-exists-p (concat root ".git")))
+      (magit-init root))
+    (cperl-project-starter-build-gitignore :root root :name project-name)
+    (cperl-project-starter-build-dist.ini :root root :name project-name)
+    (cperl-project-starter-build-Changes :root root :name project-name)
+    (cperl-project-starter-build-eproject :root root)
 
-          (magit-run-git "commit" "-m" "project boilerplate added"))
+    (magit-run-git "commit" "-m" "project boilerplate added")
 
-        (ignore-errors (eproject-reinitialize-project))
-        (eproject-maybe-turn-on)))))
+    (ignore-errors (eproject-reinitialize-project))
+    (eproject-maybe-turn-on)))
 
 (defun cperl-project-starter-git-init (root)
   (shell-command (format "cd %s; git init" root)))
 
 (defmacro* cperl-project-starter-make-file ((root file) &body forms)
   (declare (indent 1))
-  `(with-temp-buffer
-     ,@forms
-     (write-file (concat ,root ,file))
-     (magit-run-git "add" (concat ,root ,file))))
+  `(when (not (file-exists-p (concat ,root ,file)))
+     (with-temp-buffer
+       ,@forms
+       (write-file (concat ,root ,file))
+       (magit-run-git "add" (concat ,root ,file)))))
 
 (defun* cperl-project-starter-build-Makefile.PL (&key root all-from name)
   (cperl-project-starter-make-file (root "Makefile.PL")
@@ -76,16 +89,69 @@ all_from '%s';
 WriteAll;
 " name (file-relative-name all-from root)))))
 
+
+(defun* cperl-project-starter-build-dist.ini (&key root name)
+  (cperl-project-starter-make-file (root "dist.ini")
+    (insert (concat (format "
+name = %s
+author = Jonathan Rockway <jrockway@cpan.org>
+license = Perl_5
+copyright_holder = Jonathan Rockway <jrockway@cpan.org>
+copyright_year = %s" name (elt (decode-time) 5)) "
+
+[AutoPrereqs]
+
+[AutoVersion]
+
+[PkgVersion]
+
+[MetaConfig]
+
+[NextRelease]
+format = %-9v    %{EEE LLL d hh:mm:ss vvv YYYY}d
+
+[PodSyntaxTests]
+
+[PodWeaver]
+
+[Repository]
+
+[GatherDir]
+
+[PruneCruft]
+
+[ManifestSkip]
+
+[MetaYAML]
+
+[License]
+
+[Readme]
+
+[ExtraTests]
+
+[ExecDir]
+
+[ShareDir]
+
+[MakeMaker]
+
+[Manifest]
+
+[TestRelease]
+
+[@Git]
+tag_format  = %v
+tag_message = %v CPAN release
+
+[ConfirmRelease]
+
+[UploadToCPAN]
+"))))
+
 (defun* cperl-project-starter-build-gitignore (&key root name)
   (cperl-project-starter-make-file (root ".gitignore")
     (insert (format "cover_db
-META.yml
-Makefile
-blib
-inc
-pm_to_blib
-MANIFEST
-Makefile.old
 TAGS
 %s*" name))))
 
@@ -105,7 +171,7 @@ TAGS
 
 (defun* cperl-project-starter-build-Changes (&key root name)
   (cperl-project-starter-make-file (root "Changes")
-    (insert (format "Change history for %s\n" name))))
+    (insert (format "Change history for %s\n{{$NEXT}}\n" name))))
 
 (defun* cperl-project-starter-build-eproject (&key root)
   (cperl-project-starter-make-file (root ".eproject")
